80162e93ecf5216dda21d2d7a56d3cec39054bdf
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-02-05 11:41:53 +0530

    Worked on grid view for Tic-tac-Toe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import Players from "./components/Players";
+import GameBoard from "./components/GameBoard";
+
 function App() {
-  return <p>Coming Soon...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Players initialName={"Player 1"} symbol={"X"} />
+          <Players initialName={"Player 2"} symbol={"Y"} />
+        </ol>
+        <GameBoard />
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Players.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export default function Players({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
